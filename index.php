e0251e1cba51a62fcb5eb4bc6261fa65c79d90ef
MateK-web <matekuti7@gmail.com>
2022-11-08 21:18:17 +0100

    commit

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Kezdőlap <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#kezdo">Kezdőlap <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -66,6 +66,7 @@
     <span class="sr-only">Next</span>
   </a>
     </div>
+    <a id="kezdo">
     <section class="my-4">
         <div class="py-4">
             <h2 class="text-center">Kezdőlap</h2>
@@ -73,27 +74,27 @@
         <div class="container-fluid">
             <div class="row">
         <div class="col-lg-4 col-md-4 col-12">     
-        <img src="képek/info.png" class="img-fluid pb-3"> 
+        <img class="kozep" src="képek/info.png" class="img-fluid pb-3"> 
             <h3 class="text-center">Rólunk</h3>
-            <p class="text-center">Alapítványunk 1993-ban alakult. Legfőbb feladatunk a menhely fenntartása és működtetése. Kb 300 - 320 kutya és 60 - 80 cica napi ellátásáról (etetés, állatorvos, oltások) gondoskodunk. 
+            <p class="szkozep">Alapítványunk 1993-ban alakult. Legfőbb feladatunk a menhely fenntartása és működtetése. Kb 300 - 320 kutya és 60 - 80 cica napi ellátásáról (etetés, állatorvos, oltások) gondoskodunk. 
                 A körzetben jelentős a szerepünk. Nyilvántartást vezetünk a talált és az elveszett állatokról.
                 Legfőbb nehézségeink a kutyák gazdához kihelyezése és az anyagiak megteremtése. Folyamatosan frissítjük az oldalunkat, mégis a személyes látogatást javasoljuk a felelősségteljes kiválasztáshoz. 
-                Nagyon szeretnénk, ha a kutyáink sorsa jóra fordulna és szerető családra találnának.</p>
+                Nagyon szeretnénk, ha a kutyáink sorsa jóra fordulna és szerető családra találnának.<br>Ez az új oldalunk, jelenleg még csak béta fázisban jár. Esetleges opciók nem jelennek meg akkor látogason el a régi oldalunkra amelyet ezen a linken megtehet</p>
         </div>
         <div class="col-lg-4 col-md-4 col-12">     
-        <img src="képek/info.png" class="img-fluid pb-3"> 
+        <img class="kozep" src="képek/info.png" class="img-fluid pb-3"> 
             <h3 class="text-center">Támogatás</h3>
-            <img src="képek/1sz.jpg" class="img-fluid pb-3">
+            <p style="text-align:center; padding-top: 20px;"><img src="képek/1sz.jpg" class="img-fluid pb-3" ></p>
             <p class="text-center"></p>
         </div>  
         <div class="col-lg-4 col-md-4 col-12">     
-        <img src="képek/info.png" class="img-fluid pb-3"> 
+        <img class="kozep" src="képek/info.png" class="img-fluid pb-3"> 
             <h3 class="text-center">Segítsen</h3>
-            <p class="text-center">Hálásan köszönjük, ha állatorvosok jelentkeznek orvosi munkák elvégzésére. Szeretnénk az ivartalanító műtétek számát optimális szintre. <br>
+            <p class="szkozep">Hálásan köszönjük, ha állatorvosok jelentkeznek orvosi munkák elvégzésére. Szeretnénk az ivartalanító műtétek számát optimális szintre. <br>
                 Szívesen fogadunk száraz kenyeret, jó minőségű állateledelt, faforgácsot, tisztítószert, tisztítóeszközöket, gumikesztyűt, építőanyagot a menhely támogatására. </p>
         </div>  
     </section>
-
+ </a>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
